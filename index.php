3bd34403b70de5a92c3dacbf938ec83f03c1abfe
rahid <rahidmiah4@gmail.com>
2024-02-09 15:07:31 +0000

    temp commit

--- index.php
+++ index.php
@@ -3,8 +3,18 @@ session_start();
 
 $view = new stdClass();
 $view->pageTitle = 'Homepage';
-if (isset($_SESSION['login'])){
 
+$connect = mysqli_connect("poseidon.salford.ac.uk", "aee805", "Polyjuice23!", "aee805");
+$query = "SELECT usertype FROM delivery_users WHERE username LIKE '%" . $_SESSION['login'] . "%'";
+$result = mysqli_query($connect, $query);
+$row = mysqli_fetch_assoc($result);
+if ($row){
+    $usertype = $row['usertype'];
+}
+if (isset($_SESSION['login'])){
+    if ($usertype == 1){
+
+    }
 //    require_once ('loginControl.php');
 
 //    require_once ('Views/index.phtml');
